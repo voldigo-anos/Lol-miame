e1d304c4402d2424a8f87df1f2f244061cdb602c
Ere'rious 寒 <zenitsuakaza609@gmail.com>
2025-12-18 11:19:05 +0600

    Create fakechat.js

---
+++ scripts/cmds/fakechat.js
@@ -0,0 +1,62 @@
+const axios = require("axios");
+
+module.exports = {
+  config: {
+    name: "fakechat",
+    aliases: ["fc"],
+    version: "1.0",
+    author: "♡︎ 𝐻𝐴𝑆𝐴𝑁 ♡︎",
+    countDown: 5,
+    role: 0,
+    shortDescription: "fakechat",
+    longDescription: "generate fakechat",
+    category: "fun",
+    guide: {
+      en: "{pn} @tag or reply to a message"
+    }
+  },
+
+  langs: {
+    en: {
+      noTag: "you must tag or reply to a message",
+    }
+  },
+
+  onStart: async function ({ event, message, usersData, args, getLang }) {
+    try {
+      const text = args.join(" ");
+      let uid = Object.keys(event.mentions || {})[0];
+
+      if (!uid && event.type === "message_reply") {
+        uid = event.messageReply.senderID;
+      }
+
+      if (!uid) return message.reply(getLang("noTag"));
+
+      if (uid === "100068909067279") {
+        return message.reply("Muri khaw baby");
+      }
+
+      const avatar = await usersData.getAvatarUrl(uid);
+      
+      const hasan = global.GoatBot.config.api.hasan;
+
+      const imgurl = await axios.get(`${hasan}/imgbb?imageUrl=${encodeURIComponent(avatar)}`);
+
+      const pic = imgurl.data.imageUrl;
+      const userName = await usersData.getName(uid);
+      
+
+      const response = await axios.get(
+        `${hasan}/fc?msg=${encodeURIComponent(text)}&name=${encodeURIComponent(userName)}&url=${encodeURIComponent(pic)}`,
+        { responseType: "stream" }
+      );
+
+      message.reply({ attachment: response.data });
+
+    } catch (error) {
+      console.error("API Request Failed:", error);
+      message.reply("fetching some error please try again later");
+    }
+  }
+};
